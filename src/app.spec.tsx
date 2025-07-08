37701e0e5017240e932847fdea2680244726b67e
Adrian Américo <1fnadrian1@gmail.com>
2025-07-08 19:58:35 -0300

    chore: add prettier and eslint initial config and vitest

---
+++ src/app.spec.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render } from '@testing-library/react';
+import App from './App';
+
+describe('App Component', () => {
+  afterEach(cleanup);
+
+  const makeSut = () => {
+    const sut = render(
+      // <BrowserRouter>
+      <App />
+      // </BrowserRouter>
+    );
+    return { sut };
+  };
+
+  it('renders correctly', () => {
+    const { sut } = makeSut();
+
+    expect(sut.container).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@types': '/src/@types',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__test__/global-setup.ts'],
+    exclude: ['**/node_modules/**', '*/__test__/*', 'dist'],
+    coverage: {
+      all: true,
+      exclude: [
+        '**/node_modules/**',
+        '*/__test__/*',
+        'dist',
+        'commitlint.config.cjs',
+        'vite.config.ts',
+        '/src/main.tsx',
+        'vite-env.d.ts',
+        '**/*.d.ts',
+        'src/main.tsx',
+        'src/@types/*',
+        '**/index.ts',
+        'src/pages/auth/**',
+        'vitest.config.ts',
+      ],
+    },
+  },
+});
