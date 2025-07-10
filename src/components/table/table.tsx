ea16eb018f2b8d079fa9c779b8031e01709c75d2
Adrian Américo <1fnadrian1@gmail.com>
2025-07-09 21:13:33 -0300

    refactor: update table design

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,24 +1,47 @@
-import { ReactNode } from 'react';
+// Table.tsx
+import { HTMLAttributes, ReactNode } from 'react';
 import styles from './table.module.scss';
 
-export interface Column<T = any[]> {
+export interface Column<T = any> {
   key: string;
   title: string;
   render?: (row: T) => ReactNode;
 }
 
-interface TableProps<T> {
-  data: T[];
-  columns: Array<Column<T>>;
+export type TableColumnProps = {
+  title: string;
+  key: string;
+  options?: {
+    customBodyRender: (
+      data?: any,
+      row?: Record<string, any>
+    ) => string | number | ReactNode;
+  };
+};
+
+export interface TableProps extends Partial<HTMLAttributes<HTMLTableElement>> {
+  columns: TableColumnProps[];
+  data: Array<Record<string, any>>;
+  paginate?: boolean;
+  linearPagination?: boolean;
+  getPaginate?: (perPage?: number, page?: number) => void;
+  total?: number;
+  page?: number;
+  perPage?: number;
+  lastPage?: number;
+  isLoading?: boolean;
+  emptyText?: string;
+  skeletonRows?: number;
+  haveFocus?: boolean;
 }
 
-export function Table<T>({ data, columns }: TableProps<T>) {
+export const Table = ({ data, columns }: TableProps) => {
   return (
-    <div className={styles.table__wrapper}>
+    <div className={styles.table__container}>
       <table className={styles.table}>
         <thead className={styles.table__head}>
           <tr className={styles.table__row}>
-            {columns?.map((col) => (
+            {columns.map((col) => (
               <th key={col.key} className={styles.table__header}>
                 {col.title}
               </th>
@@ -27,21 +50,23 @@ export function Table<T>({ data, columns }: TableProps<T>) {
         </thead>
 
         <tbody className={styles.table__body}>
-          {data.map((row, index) => (
-            <tr
-              key={index}
-              className={`${styles.table__row} ${styles['table__row--responsive']}`}
-            >
-              {columns?.map((col) => (
-                <td key={col.key} className={styles.table__cell}>
-                  <span className={styles.table__label}>{col.title}:</span>
-                  {col.render ? col.render(row) : col.title}
-                </td>
-              ))}
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex} className={styles.table__row}>
+              {columns.map((col, colIndex) => {
+                const cellValue = row[col.key];
+
+                return (
+                  <td key={colIndex} className={styles.table__cell}>
+                    {col.options?.customBodyRender
+                      ? col.options.customBodyRender(cellValue, row)
+                      : cellValue}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
--- src/pages/employer/employer.view.tsx
+++ src/pages/employer/employer.view.tsx
@@ -1,32 +1,51 @@
 import { Header } from '@/components/header';
 import styles from './employer.module.scss';
-import { Input } from '@/components';
+import { Input, Table, TableColumnProps } from '@/components';
+import { mergeClassNames } from '@/utils';
+
+const TableColumns: TableColumnProps[] = [
+  {
+    key: 'photo',
+    title: 'Foto',
+    options: {
+      customBodyRender: (data) => (
+        <div className={styles.photoWrapper}>
+          <img src={data} alt="Foto do Funcionário" />
+        </div>
+      ),
+    },
+  },
+  { key: 'name', title: 'Nome' },
+  { key: 'position', title: 'Cargo' },
+  { key: 'admissionDate', title: 'Data de Admissão' },
+  { key: 'phone', title: 'Telefone' },
+];
+
+const mockData = [
+  {
+    photo: 'foto1.png',
+    name: 'João Silva',
+    position: 'Desenvolvedor',
+    admissionDate: '2022-01-10',
+    phone: '(81) 99999-9999',
+  },
+];
 
 export const EmployerView = () => {
   return (
     <>
       <Header />
 
-      <main className={styles.container}>
+      <main className={mergeClassNames(styles.container, 'scrollbar')}>
         <div className={styles.headerGrid}>
           <h2 className={styles.title}>Funcionários</h2>
           <div className={styles.searchWrapper}>
             <Input name="search" placeholder="Pesquisar" />
           </div>
         </div>
+
+        <Table data={mockData} columns={TableColumns} />
       </main>
     </>
-    // <div className={styles.container}>
-    //   <div className={styles.headerGrid}>
-    //     <h2 className={styles.title}>Funcionários</h2>
-    //     <div className={styles.searchWrapper}>
-    //       {/* <SearchBar
-    //         value={search}
-    //         onChange={(e) => setSearch(e.target.value)}
-    //       /> */}
-    //     </div>
-    //   </div>
-    //   {/* <EmployeeTable employees={filteredEmployees} /> */}
-    // </div>
   );
 };
